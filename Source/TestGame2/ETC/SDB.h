b2a69a0746062b128416afa175f48cedcdeb2553
cacao518 <qudwns518@naver.com>
2022-09-20 16:59:07 +0900

    - 기본 점프력, 중력스케일 상향

--- Source/TestGame2/ETC/SDB.h
+++ Source/TestGame2/ETC/SDB.h
@@ -193,7 +193,7 @@ namespace Const
     // Common
 	constexpr float ANIM_LERP_MULITPLIER      = 5.f;       // 애니메이션 중 이동 속도 보간 값
 	constexpr float DEFAULT_MOVE_SPEED        = 450.f;     // 기본 이동속도
-	constexpr float DEFAULT_JUMP_POWER        = 600.f;     // 기본 점프력
+	constexpr float DEFAULT_JUMP_POWER        = 750.f;     // 기본 점프력
 	constexpr float DEFAULT_ROTATION_RATE     = 1000.f;    // 기본 캐릭터 방향 전환 비율
 	constexpr float FLY_ROTATION_RATE         = 100.f;     // 날기 캐릭터 방향 전환 비율
 	constexpr float READY_SKILL_ROTATION_RATE = 100000.f;  // 발동 대기 스킬 보유 캐릭터 방향 전환 비율
@@ -202,7 +202,7 @@ namespace Const
 	constexpr float DEFAULT_TARGET_ARM_LENGTH = 280.f;     // 기본 카메라 암 길이
 	constexpr float FLY_TARGET_ARM_LENGTH     = 400.f;     // 날기 카메라 암 길이
 
-	constexpr float DEFAULT_GRAVITY_SCALE     = 1.5f;      // 기본 중력
+	constexpr float DEFAULT_GRAVITY_SCALE     = 2.0f;      // 기본 중력
 	constexpr float FLY_GRAVITY_SCALE         = 0.3f;      // 날기 중력
 
 	constexpr float LOCKON_RANGE = 800.f;                  // 락온 범위
--- Source/TestGame2/Manager/DataInfoManager.cpp
+++ Source/TestGame2/Manager/DataInfoManager.cpp
@@ -21,7 +21,7 @@ DataInfoManager::~DataInfoManager()
 void DataInfoManager::DataCreate()
 {
 	MaterialInfos.Add( EMaterialState::JELLY,  MaterialInfo{ 1.0f, 1.0f, 1.0f, 1.0f, 100.f, "Pawn", "/Game/StarterContent/Materials/M_Basic_Floor.M_Basic_Floor" } );
-	MaterialInfos.Add( EMaterialState::GRASS,  MaterialInfo{ 1.3f, 1.1f, 1.5f, 0.3f, 100.f, "Pawn", "/Game/StarterContent/Materials/M_Ground_Moss.M_Ground_Moss" } );
+	MaterialInfos.Add( EMaterialState::GRASS,  MaterialInfo{ 1.3f, 1.1f, 1.3f, 0.3f, 100.f, "Pawn", "/Game/StarterContent/Materials/M_Ground_Moss.M_Ground_Moss" } );
 	MaterialInfos.Add( EMaterialState::GRAVEL, MaterialInfo{ 0.9f, 0.8f, 0.8f, 1.2f, 100.f, "Pawn", "/Game/StarterContent/Materials/M_Ground_Gravel.M_Ground_Gravel" } );
 	MaterialInfos.Add( EMaterialState::ROCK,   MaterialInfo{ 0.4f, 0.5f, 0.5f, 3.0f, 100.f, "Pawn", "/Game/StarterContent/Materials/M_Rock_Basalt.M_Rock_Basalt" } );
 	MaterialInfos.Add( EMaterialState::WATER,  MaterialInfo{ 1.1f, 1.0f, 0.7f, 0.2f, 30.f, "Water", "/Game/StarterContent/Materials/M_Water_Lake.M_Water_Lake" } );
