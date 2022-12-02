907bf6519a1a9c25e88d317e30c1bea94ac09072
cacao518 <qudwns518@naver.com>
2022-12-02 14:42:28 +0900

    feat : UAnimNotify_SpawnActor 스폰 위치 SpawnPosComp 위치로 소환호도록 수정 - 네비메쉬 업데이트 런타임에 반영되도록 변경

--- Source/TestGame2/AnimNotify/AnimNotify_SpawnActor.cpp
+++ Source/TestGame2/AnimNotify/AnimNotify_SpawnActor.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AnimNotify_SpawnActor.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "../Manager/ObjectManager.h"
 
 FString UAnimNotify_SpawnActor::GetNotifyName_Implementation() const
@@ -13,6 +14,13 @@ void UAnimNotify_SpawnActor::Notify( USkeletalMeshComponent* MeshComp, UAnimSequ
 	if( !MeshComp || !( MeshComp->GetOwner() ) )
 		return;
 
+	auto spawnPosComp = Cast<USceneComponent>( MeshComp->GetOwner()->GetDefaultSubobjectByName( TEXT( "SpawnPosComp" ) ) );
+	if( !spawnPosComp )
+		return;
+
+	FVector relativeSpawnPos = spawnPosComp->GetRelativeLocation() + Pos;
+	FVector worldSpawnPos = UKismetMathLibrary::TransformLocation( spawnPosComp->GetComponentTransform(), relativeSpawnPos );
+
 	if( ObjectManager::IsVaild() )
-		GetObjectManager().SpawnActor( Actor, MeshComp->GetOwner()->GetActorLocation(), MeshComp->GetOwner()->GetActorRotation() + Rotate );
+		GetObjectManager().SpawnActor( Actor, worldSpawnPos, MeshComp->GetOwner()->GetActorRotation() + Rotate );
 }
